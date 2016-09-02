1be02bea5bbc7eb5bc867818311516e94d51da82
Jun Wan <jwan@humnet.ucla.edu>
2016-09-02 16:27:35 -0700

    CCLE-5756 Add tooltip for export options - add tooltip for export options

--- lang/en/turnitintooltwo.php
+++ lang/en/turnitintooltwo.php
@@ -577,5 +577,10 @@ $string['diagnosticoptions_0'] = 'Off';
 $string['diagnosticoptions_1'] = 'Standard';
 $string['diagnosticoptions_2'] = 'Debugging';
 // START UCLA MOD: CCLE-5751-Fix-turnitinV2-assignment-label-and-grading-text.
-$string['assignmentdefault'] = 'Assignment default';
+$string['assignmentdefault'] = 'Assignment defaults';
 // END UCLA MOD: CCLE-5751-Fix-turnitinV2-assignment-label-and-grading-text.
+// START UCLA MOD: CCLE-5756-add-tooltip-to-export-option.
+$string['exportexcel'] = 'Export to Excel';
+$string['exportpdf'] = 'Export to PDF';
+$string['exportoriginal'] = 'Export to original format';
+// END UCLA MOD: CCLE-5756-add-tooltip-to-export-option.

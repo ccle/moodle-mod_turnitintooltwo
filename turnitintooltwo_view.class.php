5f7a86a9142d04da8e449781e699d3254434cebe
Rex Lorenzo <rex@oid.ucla.edu>
2017-05-03 14:57:08 -0700

    CCLE-6336 - Turnitin Tutors show all those with grading capability

--- turnitintooltwo_view.class.php
+++ turnitintooltwo_view.class.php
@@ -2070,13 +2070,13 @@ class turnitintooltwo_view {
     public function show_add_tii_tutors_form($cm, $tutors) {
         global $CFG, $OUTPUT;
 
-        // START UCLA-MOD: CCLE-5544 Improve Turnitintwo submit button
+        // START UCLA-MOD: CCLE-6336 - Turnitin Tutors show all those with grading capability
         /* $moodletutors = get_users_by_capability(context_module::instance($cm->id), 'mod/turnitintooltwo:grade',
                                                         'u.id, u.firstname, u.lastname, u.username');
          */           
            $course = turnitintooltwo_assignment::get_course_data($cm->course);
-           $moodletutors = local_ucla_core_edit::get_course_graders($course);
-        // END UCLA-MOD: CCLE-5544 Improve Turnitintwo submit button
+           $moodletutors = local_ucla_core_edit::get_course_graders($course, '', 'mod/turnitintooltwo:grade');
+        // END UCLA-MOD: CCLE-6336
 
         // Populate elements array which will generate the form elements
         // Each element is in following format: (type, name, label, helptext (minus _help), options (if select).

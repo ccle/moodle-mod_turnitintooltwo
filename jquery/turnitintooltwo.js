7b4e9120599021965467ec7e6cfc30d2e9c92ecc
tatikamsetti <tatikamsetti@users.noreply.github.com>
2017-06-09 16:43:01 -0700

    CCLE-6620- Fixed the broken page for turnitin core changes.

--- jquery/turnitintooltwo.js
+++ jquery/turnitintooltwo.js
@@ -422,25 +422,11 @@
         // Reposition links/divs.
         $('.tii_table_functions').each(function () {
             var part_id = $(this).attr('id').split('tii_table_functions_')[1];
-
-    // Open an iframe light box containing the Peermark Manager
-    if ($('.tii_peermark_manager_launch').length > 0) {
-        $('.tii_peermark_manager_launch').colorbox({
-            iframe:true, width:"802px", height:"772px", opacity: "0.7", className: "peermark_manager", transition: "none",
-            onLoad: function() {
-                smallTiiCloseButton();
-                getLoadingGif();
-            },
-            onCleanup:function() {
-                $('#tii_close_bar').remove();
-                hideLoadingGif();
-            },
-            onClosed:function() {
-                var idStr = $(this).attr("id").split("_");
-                refreshPeermarkAssignments(idStr[2], 1);
-            }
-        });
-    }
+            var tii_table_functions = $("#tii_table_functions_" + part_id).html();
+            
+            $('#tii_table_functions_' + part_id).remove();
+            $('#' + part_id + '_length').after(tii_table_functions);
+            $('.messages_inbox').show();
 
             $('#refresh_' + part_id).show();
             $('#refreshing_' + part_id).hide();
@@ -580,19 +566,19 @@
             });
         }
 
-        // Open an iframe light box containing the Peermark Manager.
+        // Open an iframe light box containing the Peermark Manager
         if ($('.tii_peermark_manager_launch').length > 0) {
             $('.tii_peermark_manager_launch').colorbox({
-                iframe: true, width: "802px", height: "772px", opacity: "0.7", className: "peermark_manager", transition: "none",
-                onLoad: function () {
-                    lightBoxCloseButton();
+                iframe:true, width:"802px", height:"772px", opacity: "0.7", className: "peermark_manager", transition: "none",
+                onLoad: function() {
+                    smallTiiCloseButton();
                     getLoadingGif();
                 },
-                onCleanup: function () {
+                onCleanup:function() {
                     $('#tii_close_bar').remove();
                     hideLoadingGif();
                 },
-                onClosed: function () {
+                onClosed:function() {
                     var idStr = $(this).attr("id").split("_");
                     refreshPeermarkAssignments(idStr[2], 1);
                 }
@@ -1100,18 +1086,28 @@
             });
         }
 
-    function lightBoxCloseButton() {
-        // START UCLA MOD: CCLE-5834 Turnitin V2 close button.
-        // $('body').append('<div id="tii_close_bar"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
-        $('#cboxWrapper').append('<div id="tii_close_bar"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
-        // END UCLA MOD: CCLE-5834 Turnitin V2 close button.
-    }
-    
-    // START UCLA MOD: CCLE-6297 Launch peermark manager close button.
-    function smallTiiCloseButton() {
-        $('#cboxWrapper').append('<div id="small_tii_close_button"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
-    }
-    // END UCLA MOD
+        function lightBoxCloseButton() {
+            // START UCLA MOD: CCLE-5834 Turnitin V2 close button.
+            // $('body').append('<div id="tii_close_bar"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
+            $('#cboxWrapper').append('<div id="tii_close_bar"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
+            // END UCLA MOD: CCLE-5834 Turnitin V2 close button.
+        }
+        
+        // START UCLA MOD: CCLE-6297 Launch peermark manager close button.
+        function smallTiiCloseButton() {
+           $('#cboxWrapper').append('<div id="small_tii_close_button"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
+        }
+        // END UCLA MOD
+  
+
+        function initialiseUploadBox(scope, submission_id, part_id, user_id) {
+            var identifier = ".upload_box";
+            if (scope == "row") {
+                identifier = "#upload_" + submission_id + "_" + part_id + "_" + user_id;
+            }
+
+            var colorBoxWidth = "80%";
+            var colorBoxHeight = "80%";
 
             $(identifier).colorbox({
                 onLoad: function () {

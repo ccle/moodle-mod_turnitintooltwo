fef6a756278bd25b59c6524d5ad9fe15f940065a
tatikamsetti <tatikamsetti@users.noreply.github.com>
2018-06-11 16:33:14 -0700

    CCLE-7585 - Submit buttons are poorly styled.
    Reverted back to original plugin code to fix button class.

--- turnitintooltwo_view.class.php
+++ turnitintooltwo_view.class.php
@@ -1403,22 +1403,12 @@ class turnitintooltwo_view {
                 }
             }
 
-            // START UCLA-MOD: CCLE-5544 Improve Turnitintwo submit button
-
-            /*
             $upload = html_writer::link($CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&part='.$partid.'&user='.
-                                        $submission->userid.'&do=submitpaper&view_context=box_solid', $uploadtext.' '.
-                                        html_writer::tag('i', '', array('class' => 'fa fa-cloud-upload fa-lg')),
-                                        array("class" => "upload_box nowrap", "id" => "upload_".$submission->submission_objectid.
-                                                            "_".$partid."_".$submission->userid, 'data-eula' => $eulaaccepted, 'data-user-type' => $istutor));
-            */
-            $upload = html_writer::link($CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&part='.$partid.'&user='.
-                                        $submission->userid.'&do=submitpaper&view_context=box_solid', 
-                                        html_writer::tag('div', $submissionstr, array('class'=>'btn btn-default btn-primary btn-block')),
-                                        array("class" => "upload_box nowrap", "id" => "upload_".$submission->submission_objectid.
-                                                            "_".$partid."_".$submission->userid, 'data-eula' => $eulaaccepted, 'data-user-type' => $istutor));
-
-            // END UCLA-MOD: CCLE-5544 Improve Turnitintwo submit button
+                                    $submission->userid.'&do=submitpaper&view_context=box_solid', $uploadtext.' '.
+                                    html_writer::tag('i', '', array('class' => 'fa fa-cloud-upload fa-lg')),
+                                    array("class" => "upload_box nowrap",
+                                            "id" => "upload_".$submission->submission_objectid."_".$partid."_".$submission->userid,
+                                            'data-eula' => $eulaaccepted, 'data-user-type' => $istutor));
 
             if (time() > $parts[$partid]->dtdue && $turnitintooltwoassignment->turnitintooltwo->allowlate == 0 && !$istutor) {
                 $upload = "&nbsp;";
